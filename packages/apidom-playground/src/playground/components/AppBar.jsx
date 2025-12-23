565ec44b8e52ca243a3afd0194bf5492d9f15896
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-12-23 13:40:42 +0100

    feat(playground): add SpecLynx branding

--- packages/apidom-playground/src/playground/components/AppBar.jsx
+++ packages/apidom-playground/src/playground/components/AppBar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import MUIAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import speclynxLogo from '../../../public/assets/images/speclynx-logo.svg';
 
 const StyledMUIAppBar = styled(MUIAppBar)(({ theme }) => {
   return {
@@ -10,11 +12,19 @@ const StyledMUIAppBar = styled(MUIAppBar)(({ theme }) => {
   };
 });
 
+const Logo = styled('img')({
+  height: '40px',
+  marginRight: '16px',
+});
+
 const AppBar = () => {
   return (
     <StyledMUIAppBar position="sticky">
       <Toolbar>
-        <Typography variant="h6">ApiDOM Playground</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Logo src={speclynxLogo} alt="SpecLynx Logo" />
+          <Typography variant="h6">ApiDOM Playground</Typography>
+        </Box>
       </Toolbar>
     </StyledMUIAppBar>
   );
