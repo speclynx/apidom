d12841c92906668d19f859626aff7c6b8d71a4be
Vladimir Gorej <vladimir.gorej@gmail.com>
2026-02-07 00:37:23 +0100

    fix: wrapper

--- packages/apidom-reference/src/parse/parsers/arazzo-yaml-1/source-descriptions.ts
+++ packages/apidom-reference/src/parse/parsers/arazzo-yaml-1/source-descriptions.ts
@@ -1,1 +1,16 @@
-export { parseSourceDescriptions } from '../arazzo-json-1/source-descriptions.ts';
+import { ParseResultElement } from '@speclynx/apidom-datamodel';
+
+import { parseSourceDescriptions as parseSourceDescriptionsBase } from '../arazzo-json-1/source-descriptions.ts';
+import type { ReferenceOptions } from '../../../options/index.ts';
+
+/**
+ * @public
+ */
+export const parseSourceDescriptions = (
+  parseResult: ParseResultElement,
+  parseResultRetrievalURI: string,
+  options: ReferenceOptions,
+  parserName: string = 'arazzo-yaml-1',
+): Promise<ParseResultElement[]> => {
+  return parseSourceDescriptionsBase(parseResult, parseResultRetrievalURI, options, parserName);
+};
