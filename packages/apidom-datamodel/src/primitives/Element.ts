f6c483fde5bc925ee36ce9711817b5952b98dbcf
Vladimir Gorej <vladimir.gorej@gmail.com>
2026-02-12 10:24:49 +0100

    fix: address CRCs

--- packages/apidom-datamodel/src/primitives/Element.ts
+++ packages/apidom-datamodel/src/primitives/Element.ts
@@ -273,6 +273,9 @@ class Element implements ToValue, Equatable, Freezable {
 
   /** Unique identifier for this element. */
   get id(): Element {
+    if (this.isFrozen) {
+      return this.getMetaProperty('id', '') as Element;
+    }
     if (!this.hasMetaProperty('id')) {
       this.setMetaProperty('id', '');
     }
@@ -285,6 +288,9 @@ class Element implements ToValue, Equatable, Freezable {
 
   /** CSS-like class names. */
   get classes(): ArrayElement {
+    if (this.isFrozen) {
+      return this.getMetaProperty('classes', []) as ArrayElement;
+    }
     if (!this.hasMetaProperty('classes')) {
       this.setMetaProperty('classes', []);
     }
@@ -297,6 +303,9 @@ class Element implements ToValue, Equatable, Freezable {
 
   /** Hyperlinks associated with this element. */
   get links(): ArrayElement {
+    if (this.isFrozen) {
+      return this.getMetaProperty('links', []) as ArrayElement;
+    }
     if (!this.hasMetaProperty('links')) {
       this.setMetaProperty('links', []);
     }
@@ -460,10 +469,22 @@ class Element implements ToValue, Equatable, Freezable {
   }
 
   /**
-   * Gets a meta property, returning default value if not present.
+   * Gets a meta property.
+   *
+   * When the property doesn't exist:
+   * - With defaultValue: returns a new refracted element instance (not cached)
+   * - Without defaultValue: returns undefined
+   *
+   * Note: Each call with a default creates a new instance. Use setMetaProperty
+   * first if you need reference equality across multiple accesses.
    */
-  public getMetaProperty(name: string, defaultValue: unknown): Element {
+  public getMetaProperty(name: string, defaultValue: unknown): Element;
+  public getMetaProperty(name: string): Element | undefined;
+  public getMetaProperty(name: string, defaultValue?: unknown): Element | undefined {
     if (!this.hasMetaProperty(name)) {
+      if (defaultValue === undefined) {
+        return undefined;
+      }
       const element = this.refract(defaultValue);
       if (element && this.isFrozen) {
         element.freeze();
